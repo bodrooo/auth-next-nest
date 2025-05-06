bde98be29dfdb9fdb2f518c7d7757e40aa8fd837
bodrooo <bodroadikoro345@gmail.com>
2025-05-06 12:45:34 +0700

    feat(config): init @nestjs/config module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from './logger/logger.module';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoggerModule, CommonModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    LoggerModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
