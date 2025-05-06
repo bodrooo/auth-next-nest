e8fe52b15ee4ae6b79a14d4cbe8045009233518a
bodrooo <bodroadikoro345@gmail.com>
2025-05-06 12:25:06 +0700

    feat(common): init common module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from './logger/logger.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [LoggerModule, CommonModule],
   controllers: [],
   providers: [],
 })
