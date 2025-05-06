8706c31d1108c777ab53ef6139c75c8b01cf608c
bodrooo <bodroadikoro345@gmail.com>
2025-05-06 10:48:32 +0700

    chore(nest-cli),feat(logger): create nest-cli.json, init logger module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule],
   controllers: [],
   providers: [],
 })
