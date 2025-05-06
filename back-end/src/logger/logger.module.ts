7263c57fbd5c84c8e41aeac3997a8da88e376160
bodrooo <bodroadikoro345@gmail.com>
2025-05-06 12:22:22 +0700

    fix(logger): fix logger transport register

--- back-end/src/logger/logger.module.ts
+++ back-end/src/logger/logger.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { WINSTON_TRANSPORT, WinstonTransport } from './winston/transport';
+import { WINSTON_LOGGER, WinstonLogger } from './winston/logger';
 
 @Module({
   providers: [
     {
-      useClass: WinstonTransport,
       provide: WINSTON_TRANSPORT,
-      useFactory: (transportProvider: WinstonTransport) =>
-        transportProvider.TRANSPORT,
+      useFactory: () => {
+        const transportProvider = new WinstonTransport();
+        return transportProvider.TRANSPORT;
+      },
+    },
+    {
+      provide: WINSTON_LOGGER,
+      useClass: WinstonLogger,
     },
   ],
 })
