b7bb7420483aadec781979a3bbf91b6e25e419e2
bodrooo <bodroadikoro345@gmail.com>
2025-05-06 14:28:09 +0700

    feat(prisma): crate base user table

---
+++ back-end/prisma/migrations/20250506072740_create_base_user_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_id_idx" ON "User"("id");
